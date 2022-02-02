04919363e97ee3eceb6c399dda180ad644395279
SanjayB06 <sanjaymbharadwaj@gmail.com>
2022-02-02 11:54:17 -0800

    blueprint added

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import requests
 from __init__ import app
 from crud2.crud_app import app_crud1
 from crud2.crud_app import app_crud1
+from aboutus import aboutus
 from crud2.app_crud_api import app_crud_api
 
 # create a Flask instance
 #app = Flask(__name__)
 app.register_blueprint(app_crud1)
+app.register_blueprint(aboutus)
 
 
 # connects default URL to render index.html
@@ -19,45 +21,9 @@ def index():
     return render_template("index.html")
 
 
-
 @app.route('/stub/')
 def Stub():
     return render_template("stub.html")
-
-@app.route('/zachgreet', methods=['GET', 'POST'])
-def zachgreet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("zachary_greet.html", name=name)
-    # starting and empty input default
-    return render_template("zachary_greet.html", name="World")
-
-@app.route('/briangreet', methods=['GET', 'POST'])
-def briangreet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("briangreet.html", name=name)
-    # starting and empty input default
-    return render_template("briangreet.html", name="World")
-
-@app.route('/stanley', methods=['GET', 'POST'])
-def stanley():
-    url = "https://valorant-weapons.p.rapidapi.com/Sidearms"
-
-    headers = {
-        'x-rapidapi-host': "valorant-weapons.p.rapidapi.com",
-        'x-rapidapi-key': "f843e28f92mshd3de980258688f8p118a28jsn305a11f8326b"
-    }
-
-    response = requests.request("GET", url, headers=headers)
-
-    output = json.loads(response.text)
-
-    return render_template("stanley.html", Sidearms=output)
 
 @app.route('/Mini-labs/')
 def video():
@@ -366,24 +332,6 @@ def movieapi():
 
     return render_template("movieapi.html", Movie=output)
 
-@app.route('/leo', methods=['GET', 'POST'])
-def leo():
-
-
-    url = "https://weatherapi-com.p.rapidapi.com/current.json"
-
-    querystring = {"q":"32.7157,-117.1611"}
-
-    headers = {
-        'x-rapidapi-host': "weatherapi-com.p.rapidapi.com",
-        'x-rapidapi-key': "e0ad7aa5d6msh9dce61ba4b05901p1b8266jsn87d4e268c462"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    output = json.loads(response.text)
-
-    return render_template("leo.html", Weather=output)
 
 # runs the application on the development server
 if __name__ == "__main__":
